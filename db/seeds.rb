d015400c5a7015c5b74aeb2bada90faec483fdd7
Kyle Kowalsky <kyle.kowalsky@maxwellhealth.com>
2015-07-23 14:34:02 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,30 +10,32 @@ users = User.create([
 	{name: 'Shayna', nickname: 'MikeTyson', email: 'shayna@maxwellhealth.com'},
 	{name: 'Carrie', nickname: 'SunSplash', email: 'carrie@maxwellhealth.com'},
 	{name: 'Laura', nickname: 'LauraV', email: 'laura@maxwellhealth.com'},
-	{name: 'Kyle', nickname: 'TheGinjaNinja', email: 'kylek@maxwellhealth.com'}
+	{name: 'Kyle', nickname: 'TheGinjaNinja', email: 'kyle.kowalsky@maxwellhealth.com'}
 ])
 
 ideas = Idea.create([
-	{title: 'This is my awesome awesome idea', description: ' here is a big long description of the idea omg you guys it is so great', link: 'http://google.com', user_id: 3},
-	{title: 'Here is another idea', description: ' here is a big long description of the idea omg you guys it is so great', link: 'http://yahoo.com', user_id: 2 },
-	{title: 'neat guys here is another idea', description: ' here is a big long description of the idea omg you guys it is so great', link: 'http://maxwellhealth.com', user_id: 4}
+	{title: ' Too many Cooks', description: 'It takes a lot to make a stew:  A pinch of salt and laughter, too', link: 'http://google.com', user_id: 3},
+	{title: 'How to invent', description: '  all you have to do is think of things that people need, but which does not exist yet', link: 'http://yahoo.com', user_id: 2 },
+	{title: 'I am Vengance', description: 'I am the night', link: 'http://maxwellhealth.com', user_id: 4}
 ])
 
 comments = Comment.create([
-	{text: 'wow what a great idea!', user_id: 3, idea_id: 1},
-	{text: 'i don\'t know about this', user_id: 2, idea_id: 2},
-	{text: 'how awesome', user_id: 4, idea_id: 3},
-	{text: 'omg also we could do this thing', user_id: 4, idea_id: 1},
+	{text: 'A scoop of kids to add the spice', user_id: 3, idea_id: 1},
+	{text: 'You mean like an electric blanket mobile?', user_id: 1, idea_id: 2},
+	{text: 'Hamburger earmuffs!', user_id: 1, idea_id: 2},
+	{text: 'I am...BATMAN', user_id: 4, idea_id: 3},
+	{text: 'A dash of love to make it nice, and you’ve got', user_id: 4, idea_id: 1},
+	{text: 'Too Many Cooks', user_id: 2, idea_id: 1},
 ])
 
 morecomments = Comment.create([
-	{text: 'yeah i love this', user_id: 3, idea_id: 1, comment_id: 1},
-	{text: 'totally love this', user_id: 2, idea_id: 1, comment_id: 1},
+	{text: 'Uh, well... possibly... or, you could take something that already exists, and find a new use for it. Like..', user_id: 2, idea_id: 2, comment_id: 1},
+	{text: 'totally love this', user_id: 2, idea_id: 3, comment_id: 1},
 ])
 
 
 evenMore = Comment.create([
-	{text: 'but for real though', user_id: 2, idea_id: 1, comment_id: 5},
+	{text: 'Hamburger earmuffs!', user_id: 1, idea_id: 2, comment_id: 2},
 ])
 
 votes = Vote.create([
